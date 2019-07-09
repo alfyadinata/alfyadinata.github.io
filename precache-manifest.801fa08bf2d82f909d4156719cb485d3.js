478507512b129f3ae18b71177e260aecedb23723
alfyadinata <alfymuhammad7@gmail.com>
2019-07-09 19:19:32 +0700

    Updates

---
+++ precache-manifest.801fa08bf2d82f909d4156719cb485d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c74c06d220ece8615e1f05c90d2193",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8ac544f1ee3db577f70",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "63f0e4d9ad7a7e5f5da3",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f8ac544f1ee3db577f70",
+    "url": "/static/js/2.1155b40b.chunk.js"
+  },
+  {
+    "revision": "63f0e4d9ad7a7e5f5da3",
+    "url": "/static/js/main.87403ffa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
